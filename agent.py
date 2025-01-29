cc394b1bb00af2cf6a5851c1d9275be5376daeb5
Sneha P Pratap <snehapratap248@gmail.com>
2025-01-30 03:07:56 +0530

    agent1 done

---
+++ agent.py
@@ -0,0 +1,47 @@
+from crewai import Crew, Task, Agent
+from crewai_tools import SerperDevTool
+from langchain_ibm import WatsonxLLM
+import os
+
+os.environ["WATSONX_APIKEY"] = "<WATSONX API KEY>"
+os.environ["SERPER_API_KEY"] = "<SERPER API KEY>"
+
+parameters = {"decoding_method": "greedy", "max_new_tokens": 500}
+
+llm = WatsonxLLM(
+    model_id="meta-llama/llama-3-70b-instruct",
+    url="https://us-south.ml.cloud.ibm.com",
+    params=parameters,
+    project_id="<YOUR WATSONX.AI PROJECT ID HERE>",
+)
+
+function_calling_llm = WatsonxLLM(
+    model_id="ibm-mistralai/merlinite-7b",
+    url="https://us-south.ml.cloud.ibm.com",
+    params=parameters,
+    project_id="<YOUR WATSONX.AI PROJECT ID HERE>",
+)
+
+search = SerperDevTool()
+
+researcher = Agent(
+    llm=llm,
+    function_calling_llm=function_calling_llm,
+    role="Senior AI Researcher",
+    goal="Find promising research in the field of quantum computing.",
+    backstory="You are a veteran quantum computing researcher with a background in modern physics.",
+    allow_delegation=False,
+    tools=[search],
+    verbose=1,
+)
+
+task1 = Task(
+    description="Search the internet and find 5 examples of promising AI research.",
+    expected_output="A detailed bullet point summary on each of the topics. Each bullet point should cover the topic, background and why the innovation is useful.",
+    output_file="task1output.txt",
+    agent=researcher,
+)
+
+
+crew = Crew(agents=[researcher], tasks=[task1], verbose=1)
+print(crew.kickoff())
